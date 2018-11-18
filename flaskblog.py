49eb10189e6b41febb2088ece2bb45c6ccf969a9
katherine95 <kathiekim95@gmail.com>
2018-11-18 10:37:10 +0300

    Flask Blog

---
+++ flaskblog.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+#refers the app to itself
+app = Flask(__name__)
+
+posts = [
+    {
+        'author':'Cate chep',
+        'title':'Beautiful name',
+        'content':'What a beautiful name it is, nothing compares to it',
+        'date_posted':'November 17, 2018'
+    },
+    {
+        'author':'John Doe',
+        'title':'Beautiful name',
+        'content':'What a beautiful name it is, nothing compares to it',
+        'date_posted':'November 17, 2018'
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html', title='About')   
